adc41887b7fa840806f9bd788bfc6f8d543100c2
Rohit Padia <rohit.padia@publicissapient.com>
2023-01-23 21:32:49 +0530

    test2

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,13 +1,20 @@
 package com.jhooq.Jhooqk8s.ws;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("eks")
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello - Jhooq-k8s i Have updated the message";
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "This is for testing !!";
     }
 }
