dfad2ff539c8678ccfaf9a4cbf4558eaf9bb2b69
Rohit Padia <rohit.padia@publicissapient.com>
2023-01-24 15:57:09 +0530

    ropadia

--- src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/Jhooqk8s/ws/JhooqDockerDemoController.java
@@ -1,13 +1,22 @@
 package com.jhooq.Jhooqk8s.ws;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/eks")
 public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello - Jhooq-k8s i Have updated the message";
+
+    }
+
+    @GetMapping("/test")
+    public String test()
+    {
+        return "Yo! Iam RoPadia";
     }
 }
